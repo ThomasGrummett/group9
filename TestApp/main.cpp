a38942b7f8ba82f9102d179739ed0331bc8a3be8
Thomas Grummett <tgrummet@uwo.ca>
2018-11-08 02:00:57 -0500

    main.cpp edited online with Bitbucket
    Added weatherpanel to main

--- TestApp/main.cpp
+++ TestApp/main.cpp
@@ -11,15 +11,16 @@ int main(int argc, char *argv[])
 
     DigitalClock clock(&win);
     clock.show();
-
+	
+  	WeatherPanel weather(&win);
     // Grid layout with 4 widgets for time, weather, news and social
     QGridLayout *gridLayout = new QGridLayout;
-    QQuickWidget *qw2 = new QQuickWidget();
+    //QQuickWidget *qw2 = new QQuickWidget();
     QQuickWidget *qw3 = new QQuickWidget();
     QQuickWidget *qw4 = new QQuickWidget();
 
     gridLayout->addWidget(&clock,0,0,1,1);
-    gridLayout->addWidget(qw2,0,1,1,2);
+    gridLayout->addWidget(&weather,0,1,1,2);
     gridLayout->addWidget(qw3,1,0,2,3);
     gridLayout->addWidget(qw4,0,3,3,2);
 
