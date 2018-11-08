9a988be9a798d808caa4feea913d789a6a4683f1
Thomas Grummett <tgrummet@uwo.cagit config --global user.email tgrummet@uwo.cagit config --global user.email tgrummet@uwo.ca>
2018-11-08 01:55:32 -0500

    initial comit

---
+++ WeatherPanel.h
@@ -0,0 +1,34 @@
+#ifndef
+#define WEATHERPANEL_H
+
+#include <QLabel>
+#include <QString>
+#include <stdlib.h>
+#include <unistd.h>
+#include <curl/curl.h>
+#include <curl/types.h>
+#include <curl/easy.h>
+#include "rapidjson"
+#include <string>
+#include <fstream>
+
+class WeatherPanel: public QLabel
+{
+    private:
+		/* weather[0] holds city name
+		*weather[1] holds the weather group ie clouds
+		*weather[2] holds the weather name ie broken clouds
+		*weather[3] holds the temperature
+		*weather[4] holds the humidity
+		*/
+		string weather[5];
+		QString weatherToToDisplay();
+		QString outputToLabel;
+		void pullData();
+	public:
+		WeatherPanel(QWidget *parent = 0);
+		string[] update();
+		QLabel getWeather();
+};
+
+#endif
